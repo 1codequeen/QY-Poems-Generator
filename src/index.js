792290e87905bd6fd7437bb6e5cc0b0279909206
1codequeen <queenyona21@gmail.com>
2025-03-05 11:05:49 +0200

    fixed js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 function generatePoem(event) {
   event.preventDefault();
+  new Typewriter("#Poems", {
+    strings: ["Roses are red...", "Violets are blue..."],
+    autoStart: true,
+    delay: 75,
+    cursor: "",
+  });
 }
-
-new Typewriter("#poems", {
-  strings: ["Roses are red...", "Violets are blue..."],
-  autoStart: true,
-  delay: 75,
-  cursor: "",
-});
 
 let poemFormElement = document.querySelector("#poem-generator");
 poemFormElement.addEventListener("submit", generatePoem);
