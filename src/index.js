c73239496ed96698d43e5eb42878dec506435a28
1codequeen <queenyona21@gmail.com>
2025-03-07 19:02:11 +0200

    edited js

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ function displayPoems(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsOutput = document.querySelector("#user-instructions");
+  let instructionsOutput = document.querySelector(".instructions");
   let apiKey = "9435699d6f2a3f2cd6b3e67t0o99669e";
   let prompt = `Generate a poem about ${instructionsOutput.value}`;
   let context =
     "You are an ai poem generator that writes short poems about different types of emotions your mission is to write maximum of four lined poems that talk about each emotion";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating Poem");
   console.log(`Prompt:${prompt}`);
